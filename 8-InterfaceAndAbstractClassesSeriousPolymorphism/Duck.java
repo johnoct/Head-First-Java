7eeb9545e5950f89f59ecbcbbea915a47741cab2
John <johnoctubre7@gmail.com>
2017-07-30 17:49:09 -0400

    finished Chapter

---
+++ 8-InterfaceAndAbstractClassesSeriousPolymorphism/Duck.java
@@ -0,0 +1,8 @@
+public class Duck{
+    int size;
+    public Duck(int size){
+        this.size = size;
+        System.out.println("size is " + size);
+        System.out.println("Quack");
+    }
+}
